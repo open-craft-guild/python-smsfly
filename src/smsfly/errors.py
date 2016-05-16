5acd4d6a0e367b293305e91987c8a732dbb967ed
Anna Kurylo <anna.kurilo@outlook.com>
2016-05-16 19:24:45 +0300

    Create custom error classes

--- src/smsfly/errors.py
+++ src/smsfly/errors.py
@@ -1,38 +1,47 @@
 class XMLError(SyntaxError):
     """Incorrect XML was sent to API"""
+    pass
 
 
 class PhoneError(ValueError):
     """Incorrect phone of recipient"""
+    pass
 
 
 class StartTimeError(ValueError):
     """Incorrect start time for sending messages"""
+    pass
 
 
 class EndTimeError(ValueError):
     """Incorrect end time of campaign"""
+    pass
 
 
 class StartTimeError(ValueError):
     """Incorrect start time of campaign"""
+    pass
 
 
 class LifetimeError(ValueError):
     """Incorrect lifetime of campaign"""
+    pass
 
 
 class SpeedError(ValueError):
     """Incorrect speed for sending messages"""
 
 
-class AlphanameError(NameError):
+class AlphanameError(ValueError):
     """The name is forbidden for use, or there's an error in it"""
+    pass
 
 
 class TextError(ValueError):
     """Incorrect text of message"""
+    pass
 
 
 class InsufficientFundsError(Exception):
     """Insufficient funds. Unable to send messages"""
+    pass
