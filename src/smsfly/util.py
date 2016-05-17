fc30b4dc7f6dbdadc65ed3758207946fd73a98c6
Anna Kurylo <anna.kurilo@outlook.com>
2016-05-17 21:02:10 +0300

    Implement parsing xml responses

--- src/smsfly/util.py
+++ src/smsfly/util.py
@@ -1,10 +1,29 @@
 from functools import wraps
 from bs4 import BeautifulSoup as bs
 
+import XMLError, PhoneError, StartTimeError, EndTimeError, LifetimeError, SpeedError,
+       AlphanameError, TextError, InsufficientFundsError from errors
+
+
+ERROR_MAP = {
+    'XMLERROR': XMLError,
+    'ERRPHONES': PhoneError,
+    'ERRSTARTTIME': StartTimeError,
+    'ERRENDTIME': EndTimeError,
+    'ERRLIFETIME': LifetimeError,
+    'ERRSPEED': SpeedError,
+    'ERRALFANAME': AlphanameError,
+    'ERRTEXT': TextError,
+    'INSUFFICIENTFUNDS': InsufficientFundsError
+}
 
 def parse_xml_response(f):
     @wraps(f)
     def wrapper(*args, **kwargs):
-        # TODO: implement exception detection
-        return bs(f(*args, **kwargs), features='lxml-xml')
+        res_xml = bs(f(*args, **kwargs), features='lxml-xml')
+        state_code = res_xml.message.state['code']
+        try:
+            raise ERROR_MAP[state_code]
+        except KeyError:
+            return res_xml
     return wrapper
